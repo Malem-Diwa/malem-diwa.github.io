d9175762fb1a9219505d498ea449517e81925f32
Malem-Diwa <malem.diwa25@outlook.com>
2026-06-24 10:52:44 +0700

    Changed setInterval timer.
    After I noticed my code that has some lines not ending with ";",
    i fixed it. I added ";" ending to the lines with no ending.

--- client-side local clock.js
+++ client-side local clock.js
@@ -11,8 +11,7 @@ function update() {
     var menit = padZero(date.getMinutes());
     var detik = padZero(date.getSeconds());
     var zona_waktu = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    let datetime = window.document.getElementById("waktu")
+    let datetime = window.document.getElementById("waktu");
     datetime.innerText = "Client-Side Local Clock Time: [" + tahun + "-" + bulan + "-" + hari + " " + jam + "." + menit + "." + detik + " " + "(" + zona_waktu + ")" + "]";
 }
-
-setInterval(update, 1000)
+setInterval(update, 769);
